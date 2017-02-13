ed143f7f4458813bb380604dc2d0535c6c005033
Slaubach <launch.17@osu.edu>
2017-02-13 14:06:41 -0500

    fisherInsurance-lab3

---
+++ Controller/AutoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FisherInsurance.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+
+
+    public class AutoController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return Ok("This is the auto page");
+        }
+
+        [Route("auto/quotes")]
+        public IActionResult quotes()
+        {
+        Quote quote = new Quote
+        {
+            Id = 345,
+            Product = "Auto Insurance",
+
+            ExpireDate = DateTime.Now.AddDays(45),
+            Price = 45.00M
+        };
+
+        //return Ok("this is the quotes page");  
+        return View(quote);      
+        }
+
+    }
+
---
+++ Models/Claims.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FisherInsurance.Models
+{
+    public class Claims
+    {
+        
+            public int Id { get; set; }
+            public string PolicyType { get; set; }
+            public DateTime LossDate { get; set; }
+            public decimal LossAMount { get; set; }
+        }
+    }
+
